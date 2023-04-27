ad859d8a9cd5182304924fcbf6cc83fca8409070
DoanDucHoang <doanduckien.2001@gmail.com>
2023-04-28 00:59:32 +0700

    update file .env

--- server/connect.js
+++ server/connect.js
@@ -1,10 +1,11 @@
 import mysql from 'mysql';
-
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'vjd@2022',
-  database: 'vjc-matching',
-  multipleStatements: true
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  multipleStatements: process.env.multipleStatements,
 });

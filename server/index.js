af9feadb65d050da393efb2aa20ca9d6b1d55bc6
DoanDucHoang <doanduckien.2001@gmail.com>
2023-04-28 15:26:29 +0700

    update DB

--- server/index.js
+++ server/index.js
@@ -38,8 +38,8 @@ app.get('/', (req, res) => {
 });
 app.get('/server', (req, res) => {
   const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
+    host: '54.179.77.71',
+    user: 'hoangdoan',
     password: 'Duckien.2001',
     database: 'vjc-matching',
     multipleStatements: true,
